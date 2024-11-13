5e8549012f3bdbb7b28c4ea405106abcc59a423e
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-11-13 23:34:22 +0530

    Created basic chat page

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,14 +1,14 @@
 import React, { useContext } from "react";
 import { UserContext } from "./UserContext";
 import RegisterAndLoginForm from "./RegisterAndLoginForm";
+import Chat from "./Chat";
 
 function Routes() {
   const { username, id } = useContext(UserContext);
 
   if (username) {
-    return "logged in!" + username;
+    return <Chat />;
   }
-
   return <RegisterAndLoginForm />;
 }
 
