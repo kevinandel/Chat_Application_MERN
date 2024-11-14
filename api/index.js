38ed510d513edc06fbb0927a4f0841e2489bfcaa
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-11-14 18:10:43 +0530

    Dsiplayed on left who is online using websocket

--- api/index.js
+++ api/index.js
@@ -107,4 +107,15 @@ wss.on("connection", (connection, req) => {
       }
     }
   }
+
+  [...wss.clients].forEach((client) => {
+    client.send(
+      JSON.stringify({
+        online: [...wss.clients].map((c) => ({
+          userId: c.userId,
+          username: c.username,
+        })),
+      })
+    );
+  });
 });
