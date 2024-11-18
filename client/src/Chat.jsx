2c3a5ab9d8431b47b61e402c4398a7e1059b6cc8
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-11-18 15:18:43 +0530

    Fetched old messages from database

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -3,6 +3,7 @@ import Avatar from "./Avatar";
 import Logo from "./Logo";
 import { UserContext } from "./UserContext";
 import { uniqBy } from "lodash";
+import axios from "axios";
 
 function Chat() {
   const [ws, setWs] = useState(null);
@@ -65,6 +66,12 @@ function Chat() {
       div.scrollIntoView({ behaviour: "smooth" });
     }
   }, [messages]);
+
+  useEffect(() => {
+    if (selectedUserId) {
+      axios.get(`/messages/${selectedUserId}`);
+    }
+  }, [selectedUserId]);
 
   const onlinePeopleExclOurUser = { ...onlinePeople };
   delete onlinePeopleExclOurUser[id];
